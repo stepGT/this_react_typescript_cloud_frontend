7bec07df0b64e2bdeffac912eb3a892de80a08a1
Andrey Zuev <stepgt@mail.ru>
2023-05-05 09:20:03 +1000

    Refactor onSubmit LoginForm && Added authorization

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import styles from './Auth.module.scss';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, notification } from 'antd';
+import { ILoginFormDTO } from '@/api/dto/auth.dto';
+import { setCookie } from 'nookies';
+import * as Api from '@/api';
 
 export const LoginForm: React.FC = () => {
-  const onSubmit = (values: any) => {
-    console.log(values);
+  const onSubmit = async (values: ILoginFormDTO) => {
+    try {
+      const { token } = await Api.auth.login(values);
+
+      notification.success({
+        message: 'Успешно!',
+        description: 'Переходим в админ-панель...',
+        duration: 2,
+      });
+
+      setCookie(null, '_token', token, {
+        path: '/',
+      });
+    } catch (err) {
+      console.warn('LoginForm', err);
+
+      notification.error({
+        message: 'Ошибка!',
+        description: 'Неверный логин или пароль',
+        duration: 2,
+      });
+    }
   };
   return (
     <div className={styles.formBlock}>
